39de6082b1b304d020da0f43da71850ff2b13e9a
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-09-08 20:08:15 +0800

    Updates

---
+++ precache-manifest.bad09b15860322585725e64de04a0840.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe0dd9dbbc1f6a00f58300d5956b8c34",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "df0b69df50fab8677f18",
+    "url": "/react-portfolio/static/js/2.3e3cfecf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.3e3cfecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27fce7e07c7f60a96214",
+    "url": "/react-portfolio/static/js/main.dc68c73b.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
